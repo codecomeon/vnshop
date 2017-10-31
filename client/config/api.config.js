3cf803cd98319249b50b1c9ea7559526e5849722
WPH2017 <2974769672@qq.com>
2017-10-31 18:54:47 +0800

    调整配置

--- client/config/api.config.js
+++ client/config/api.config.js
@@ -1,5 +1,5 @@
 const isPro = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-    baseUrl: isPro ? 'http://git.wphkj.cn/' : 'api/'
+    baseUrl: isPro ? 'http://git.wphkj.cn/api/' : 'api/'
 }
