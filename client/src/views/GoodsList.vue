fec6e3a3776acc1aa339f09d04d18151442ad6c2
WPH2017 <2974769672@qq.com>
2017-11-01 16:07:00 +0800

    123

--- client/src/views/GoodsList.vue
+++ client/src/views/GoodsList.vue
@@ -8,7 +8,7 @@
                 <span class="sortby">Sort By:</span>
                 <a href="javascript:void(0)" class="default cur">默认</a>
                 <a href="javascript:void(0)" class="price" @click="sortGoods">price<svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-                <a href="javascript:void(0)" class="filterby stopPop">Sort By </a>
+                <a href="javascript:void(0)" class="filterby stopPop">Sort By</a>
             </div>
             <div class="accessory-result">
                 <!-- filter -->
