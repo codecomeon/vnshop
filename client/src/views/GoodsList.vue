a2694c7176892466484ddeca0a0c45ae6bbc41c7
WPH2017 <2974769672@qq.com>
2017-11-01 14:32:35 +0800

    好的

--- client/src/views/GoodsList.vue
+++ client/src/views/GoodsList.vue
@@ -6,9 +6,9 @@
         <div class="container">
             <div class="filter-nav">
                 <span class="sortby">Sort by:</span>
-                <a href="javascript:void(0)" class="default cur">默认</a>
-                <a href="javascript:void(0)" class="price" @click="sortGoods">价格 <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-                <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+                <a href="javascript:void(0)" class="default cur">Default</a>
+                <a href="javascript:void(0)" class="price" @click="sortGoods">price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+                <a href="javascript:void(0)" class="filterby stopPop">sort by</a>
             </div>
             <div class="accessory-result">
                 <!-- filter -->
